71889d7493ce81717caee256e18f458cd0b1bd43
safalafas <safal@ismt.edu.np>
2025-01-23 22:21:54 -0500

    Update image tag to v0.2.0 and add Azure Container Instance configuration

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,6 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from '@pulumi/pulumi'
 import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
+import * as docker from '@pulumi/docker'
+import * as containerinstance from '@pulumi/azure-native/containerinstance'
 
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
@@ -39,3 +41,78 @@ const registryCredentials = containerregistry
       password: creds.passwords![0].value!,
     }
   })
+
+// Define the container image for the service.
+const image = new docker.Image(`${prefixName}-image`, {
+  imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
+  build: {
+    context: appPath,
+    platform: 'linux/amd64',
+  },
+  registry: {
+    server: registry.loginServer,
+    username: registryCredentials.username,
+    password: registryCredentials.password,
+  },
+})
+
+// Create a container group in the Azure Container App service and make it publicly accessible.
+const containerGroup = new containerinstance.ContainerGroup(
+  `${prefixName}-container-group`,
+  {
+    resourceGroupName: resourceGroup.name,
+    osType: 'linux',
+    restartPolicy: 'always',
+    imageRegistryCredentials: [
+      {
+        server: registry.loginServer,
+        username: registryCredentials.username,
+        password: registryCredentials.password,
+      },
+    ],
+    containers: [
+      {
+        name: imageName,
+        image: image.imageName,
+        ports: [
+          {
+            port: containerPort,
+            protocol: 'tcp',
+          },
+        ],
+        environmentVariables: [
+          {
+            name: 'PORT',
+            value: containerPort.toString(),
+          },
+          {
+            name: 'WEATHER_API_KEY',
+            value: '540009e48bea9cdfe56008539550adc2',
+          },
+        ],
+        resources: {
+          requests: {
+            cpu: cpu,
+            memoryInGB: memory,
+          },
+        },
+      },
+    ],
+    ipAddress: {
+      type: containerinstance.ContainerGroupIpAddressType.Public,
+      dnsNameLabel: `${imageName}`,
+      ports: [
+        {
+          port: publicPort,
+          protocol: 'tcp',
+        },
+      ],
+    },
+  },
+)
+
+export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
+const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
+const url = containerGroup.ipAddress.apply(
+  (addr) => `http://${addr!.fqdn!}:${containerPort}`,
+)
